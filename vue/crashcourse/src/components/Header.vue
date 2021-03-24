e0940e48c77105a1c60cade31be34a58ce40e9c0
rachit2501 <rac.sri25@gmail.com>
2021-03-25 01:06:11 +0530

    components

---
+++ vue/crashcourse/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>Task Tracker {{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
