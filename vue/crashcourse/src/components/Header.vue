4e62e46a6510a7fd19952ddc6d860907bc215782
rachit2501 <rac.sri25@gmail.com>
2021-03-27 01:40:38 +0530

    strted with router

--- vue/crashcourse/src/components/Header.vue
+++ vue/crashcourse/src/components/Header.vue
@@ -1,13 +1,35 @@
 <template>
   <header>
-    <h1>Task Tracker {{ title }}</h1>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
   </header>
 </template>
 
 <script>
+import Button from "./Button";
 export default {
   name: "Header",
-  props: ["title"],
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 };
 </script>
 
@@ -15,5 +37,7 @@ export default {
 header {
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 }
 </style>
